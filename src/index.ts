e1ce2e6b593882ab553e7a23893d138bb5a22739
mauro <mfelipxl@gmail.com>
2025-03-31 14:37:55 -0300

    Feat: create a function for create user and verify if already exists.

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,6 @@ import Fastify from 'fastify'
 import userRoutes from './routes/userRoutes'
 const app = Fastify()
 
-app.addContentTypeParser('application/json', (req, payload, done) => {
-    let data = ''
-    payload.on('data', chunk => { data += chunk })
-    payload.on('end', () => {
-      try {
-        const json = JSON.parse(data)
-        done(null, json)
-      } catch (err) {
-        done(new Error('Erro ao fazer parsing do JSON'))
-      }
-    })
-})
-
 app.register(userRoutes)
 
 app.get('/', async (request, reply) => {
